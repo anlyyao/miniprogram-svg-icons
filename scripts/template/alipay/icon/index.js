e39c7b898dccf6db7ed9202be408046df7d68780
Y. <anllyyao@gmail.com>
2026-04-13 03:18:43 +0800

    chore: optimize icon comp logic

--- scripts/template/alipay/icon/index.js
+++ scripts/template/alipay/icon/index.js
@@ -13,71 +13,54 @@ Component({
   data: {
     rootStyle: '',
     svgDataUri: '',
-    _strokeColors: '',
-    _fillColors: '',
   },
 
   didMount() {
-    this.getSvgContent();
+    this._update();
   },
 
   didUpdate(prevProps) {
     if (
       prevProps.name !== this.props.name ||
-      prevProps.brand !== this.props.brand
-    ) {
-      this.getSvgContent();
-    }
-    if (
+      prevProps.brand !== this.props.brand ||
       prevProps.size !== this.props.size ||
       prevProps.strokeWidth !== this.props.strokeWidth ||
       prevProps.strokeColor !== this.props.strokeColor ||
       prevProps.fillColor !== this.props.fillColor
     ) {
-      this.init();
+      this._update();
     }
   },
 
   methods: {
-    getSvgContent() {
-      const { name, brand } = this.props;
-      const iconsData = iconsMap[brand] || {};
-      const svgContent = iconsData[name] || '';
-      this.setData({ svgContent });
-      this.init();
-    },
-
-    updateRootStyle(size) {
-      var s = typeof size === 'number' ? size + 'px' : size;
-      this.setData({ rootStyle: 'width: ' + s + '; height: ' + s });
-    },
-
-    init() {
+    _update() {
+      var name = this.props.name;
+      var brand = this.props.brand;
       var size = this.props.size;
       var strokeWidth = this.props.strokeWidth;
       var strokeColor = this.props.strokeColor;
       var fillColor = this.props.fillColor;
-      var svgContent = this.data.svgContent;
 
-      this.updateRootStyle(size);
-
-      var _strokeColors = this.normalizeColor(strokeColor);
-      var _fillColors = this.normalizeColor(fillColor);
+      var iconsData = iconsMap[brand] || {};
+      var svgContent = iconsData[name] || '';
+      var s = typeof size === 'number' ? size + 'px' : size;
+      var strokeColors = this._normalizeColor(strokeColor);
+      var fillColors = this._normalizeColor(fillColor);
+      var svg = this._buildSvg(svgContent, strokeWidth, strokeColors, fillColors);
 
       this.setData({
-        _strokeColors: _strokeColors,
-        _fillColors: _fillColors,
-        svgDataUri: this.getSvgDataUri(svgContent, strokeWidth, _strokeColors, _fillColors),
+        rootStyle: 'width: ' + s + '; height: ' + s,
+        svgDataUri: svg ? 'data:image/svg+xml;charset=utf-8,' + svg.replace(/<|>|#/g, function (m) { return encodeURIComponent(m); }) : '',
       });
     },
 
-    normalizeColor(color) {
+    _normalizeColor(color) {
       if (!color) return null;
-      if (typeof color === 'string') return this.hex2rgb(color);
-      return color.map(function (c) { return this.hex2rgb(c); }.bind(this));
+      if (typeof color === 'string') return this._hex2rgb(color);
+      return color.map(function (c) { return this._hex2rgb(c); }.bind(this));
     },
 
-    hex2rgb(hex) {
+    _hex2rgb(hex) {
       if (hex.indexOf('#') !== 0) return hex;
       hex = hex.slice(1);
       if (hex.length === 3) hex = hex.replace(/(.)/g, '$1$1');
@@ -89,22 +72,14 @@ Component({
       return 'rgb(' + rgb.join(',') + ')';
     },
 
-    getSvg(svgContent, strokeWidth, strokeColors, fillColors) {
+    _buildSvg(svgContent, strokeWidth, strokeColors, fillColors) {
       if (!svgContent) return '';
-
       var fill = [].concat(fillColors || []);
       var stroke = [].concat(strokeColors || []);
-
       return svgContent
         .replace(/\{\{fillColor(\d+)\s*\|\|\s*'([^']+)'\}\}/g, function (_, i, d) { return fill[i - 1] || d; })
         .replace(/\{\{strokeColor(\d+)\s*\|\|\s*'([^']+)'\}\}/g, function (_, i, d) { return stroke[i - 1] || d; })
         .replace(/\{\{strokeWidth\}\}/g, strokeWidth);
-    },
-
-    getSvgDataUri(svgContent, strokeWidth, strokeColors, fillColors) {
-      var svg = this.getSvg(svgContent, strokeWidth, strokeColors, fillColors);
-      if (!svg) return '';
-      return 'data:image/svg+xml;charset=utf-8,' + svg.replace(/<|>|#/g, function (m) { return encodeURIComponent(m); });
     },
   },
 });
--- scripts/template/baidu/icon/index.js
+++ scripts/template/baidu/icon/index.js
@@ -1,19 +1,9 @@
 const iconsMap = require("./icons");
 
 Component({
-  options: {
-    pureDataPattern: /^_/,
-  },
-
   properties: {
-    name: {
-      type: String,
-      value: '',
-    },
-    brand: {
-      type: String,
-      value: 'tdesign',
-    },
+    name: { type: String, value: '' },
+    brand: { type: String, value: 'tdesign' },
     size: { type: null, value: 24 },
     strokeWidth: { type: String, value: 2 },
     strokeColor: { type: null },
@@ -21,91 +11,55 @@ Component({
   },
 
   observers: {
-    'name, brand'() {
-      this.getSvgContent();
-      this.init();
+    'name, brand, size, strokeWidth, strokeColor, fillColor'() {
+      this._update();
     },
-    'strokeWidth,strokeColor,fillColor'() {
-      this.init();
-    },
-    size(s) {
-      this.updateRootStyle(s);
-    },
-  },
-
-  data: {
-    _strokeColors: '',
-    _fillColors: '',
   },
 
   lifetimes: {
     attached() {
-      this.getSvgContent();
-      this.init();
+      this._update();
     },
   },
 
   methods: {
-    getSvgContent() {
-      const { name, brand } = this.data;
+    _update() {
+      const { name, brand, size, strokeWidth, strokeColor, fillColor } = this.data;
       const iconsData = iconsMap[brand] || {};
       const svgContent = iconsData[name] || '';
-      this.setData({ svgContent });
-    },
-
-    updateRootStyle(size) {
       const s = typeof size === 'number' ? `${size}px` : size;
-      this.setData({ rootStyle: `width: ${s}; height: ${s}` });
-    },
-
-    init() {
-      const { size, strokeWidth, strokeColor, fillColor, svgContent } = this.data;
-      this.updateRootStyle(size);
-
-      const _strokeColors = this.normalizeColor(strokeColor);
-      const _fillColors = this.normalizeColor(fillColor);
+      const strokeColors = this._normalizeColor(strokeColor);
+      const fillColors = this._normalizeColor(fillColor);
+      const svg = this._buildSvg(svgContent, strokeWidth, strokeColors, fillColors);
 
       this.setData({
-        _strokeColors,
-        _fillColors,
-        svgDataUri: this.getSvgDataUri(svgContent, strokeWidth, _strokeColors, _fillColors),
+        rootStyle: `width: ${s}; height: ${s}`,
+        svgDataUri: svg ? `data:image/svg+xml;charset=utf-8,${svg.replace(/"/g, "'").replace(/<|>|#/g, (m) => encodeURIComponent(m))}` : '',
       });
     },
 
-    normalizeColor(color) {
+    _normalizeColor(color) {
       if (!color) return null;
-      if (typeof color === 'string') return this.hex2rgb(color);
-      return color.map((c) => this.hex2rgb(c));
+      if (typeof color === 'string') return this._hex2rgb(color);
+      return color.map((c) => this._hex2rgb(c));
     },
 
-    hex2rgb(hex) {
-      if (hex.indexOf('#') !== 0) return hex;
+    _hex2rgb(hex) {
+      if (hex[0] !== '#') return hex;
       hex = hex.slice(1);
       if (hex.length === 3) hex = hex.replace(/(.)/g, '$1$1');
-      const rgb = [];
-      hex.replace(/../g, (c) => {
-        rgb.push(parseInt(c, 16));
-        return c;
-      });
-      return `rgb(${rgb.join(',')})`;
+      const [r, g, b] = hex.match(/../g).map((c) => parseInt(c, 16));
+      return `rgb(${r},${g},${b})`;
     },
 
-    getSvg(svgContent, strokeWidth, strokeColors, fillColors) {
+    _buildSvg(svgContent, strokeWidth, strokeColors, fillColors) {
       if (!svgContent) return '';
-
       const fill = [].concat(fillColors || []);
       const stroke = [].concat(strokeColors || []);
-
       return svgContent
         .replace(/\{\{fillColor(\d+)\s*\|\|\s*'([^']+)'\}\}/g, (_, i, d) => fill[i - 1] || d)
         .replace(/\{\{strokeColor(\d+)\s*\|\|\s*'([^']+)'\}\}/g, (_, i, d) => stroke[i - 1] || d)
         .replace(/\{\{strokeWidth\}\}/g, strokeWidth);
-    },
-
-    getSvgDataUri(svgContent, strokeWidth, strokeColors, fillColors) {
-      const svg = this.getSvg(svgContent, strokeWidth, strokeColors, fillColors);
-      if (!svg) return '';
-      return `data:image/svg+xml;charset=utf-8,${svg.replace(/"/g, "'").replace(/<|>|#/g, (m) => encodeURIComponent(m))}`;
     },
   },
 });

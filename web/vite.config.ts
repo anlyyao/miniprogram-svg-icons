e0a4e822bfaa338a0d5088ebf4670b0acfd24452
Y. <anllyyao@gmail.com>
2026-05-07 16:46:27 +0800

    ci(surge): fixed Surge preview failure (#22)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   build: {
     outDir: '../dist/web',
   },
-  base: '/miniprogram-svg-icons/',
+  base: process.env.DEPLOY_DOMAIN ? '/' : '/miniprogram-svg-icons/',
   server: {
     port: 3000,
     host: true,
